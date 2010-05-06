3ab90ee4721c16c732932495c136cbbd961fd72c
Lucas Teixeira <lucastex@imac.local>
2010-05-06 01:31:02 -0300

    Initial commit

---
+++ Config.groovy
@@ -0,0 +1,9 @@
+twitter {
+	username = "yourtwitterusername" //twitter username
+	password = "yourtwitterpassword" //twitter password
+}
+
+queries {
+	rpp = 30 //ammt of tweets returned on each query
+	terms = ["groovy", "grails", "gradle", "gant", "grape", "griffon", "groovlet", "gaelyk", "gmaven"] //terms to search
+}
---
+++ TwitterFollowerBot.groovy
@@ -0,0 +1,48 @@
+import twitter4j.Twitter                  
+import twitter4j.TwitterFactory                  
+import twitter4j.Query
+
+log("loading config")
+def config = new ConfigSlurper().parse(new File("Config.groovy").toURL())
+
+log("connecting with username '${config.twitter.username}'")
+def twitter = new TwitterFactory().getInstance(config.twitter.username, config.twitter.password)
+
+//user list to check and follow
+def userList = []
+
+config.queries?.terms?.each { queryTerm ->
+	
+	def query = new Query(queryTerm)
+	query.setRpp(config.queries.rpp)
+	
+	def queryResult = twitter.search(query)
+	def tweets = queryResult?.tweets
+
+	log("querying for '${queryTerm}' returned ${tweets.size()} results")
+	tweets.each { tweet ->
+		if (!userList.contains(tweet.fromUser)) {
+			userList << tweet.fromUser
+		}
+	}
+}
+
+log("will try to follow ${userList.size()} new users")
+userList.each { user ->
+	
+	if (!twitter.existsFriendship(config.twitter.username, user)) {
+		try {
+			twitter.createFriendship(user, true)
+			log("Created friendship between '${config.twitter.username}' and '${user}'")
+
+		} catch (Exception exp) { 
+			log("Error trying to follow user ${user}: ${exp.message}")
+		}
+	} else {
+		log("User ${user} is already on your friends list")
+	}
+}
+
+void log(message) {
+	println "[${new Date()}] [Twitter robot] [${message}]"
+}
